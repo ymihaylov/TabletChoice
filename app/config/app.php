21c96a4e2028478e95d66de88a4d601ec556e5c0
Yavor Mihaylov <yavor.st.m@gmail.com>
2014-12-29 13:48:00 +0200

    Add generator to providers

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Bulforce\ExtDirect\ExtDirectServiceProvider',
+		'Way\Generators\GeneratorsServiceProvider',
 
 	),
 
