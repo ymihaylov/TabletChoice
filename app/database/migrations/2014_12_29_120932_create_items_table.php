8919a0b0c98c4c12288f00c34951419fa809dbbb
Yavor Mihaylov <yavor.st.m@gmail.com>
2014-12-29 14:10:59 +0200

    Add items table migration

---
+++ app/database/migrations/2014_12_29_120932_create_items_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateItemsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('items', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('category_id');
+			$table->string('name');
+			$table->string('img_url');
+			$table->string('short_description');
+			$table->string('description');
+			$table->float('price');
+			$table->string('quantity');
+			$table->timestamp('deleted_at');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('items');
+	}
+
+}
