4d38e0ceec8d77b15e503f73b9e4fde02eba423e
Yavor Mihaylov <yavor.st.m@gmail.com>
2015-01-10 19:54:55 +0200

    Add comments table seeder

---
+++ app/database/seeds/CommentsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+// Composer: "fzaninotto/faker": "v1.3.0"
+use Faker\Factory as Faker;
+
+class CommentsTableSeeder extends Seeder {
+
+	public function run()
+	{
+		$faker = Faker::create();
+
+		foreach(range(1, 10) as $index)
+		{
+			Comment::create([
+				'item_id' => $faker->numberBetween(1, 10),
+				'content' => $faker->text(),
+			]);
+		}
+	}
+
+}
