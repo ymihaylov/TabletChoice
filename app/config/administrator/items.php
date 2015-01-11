afdf213d77b2144fbda35758130e69c5ed30e154
Yavor Mihaylov <yavor.st.m@gmail.com>
2015-01-11 17:31:13 +0200

    Add items config for admin

--- app/config/administrator/items.php
+++ app/config/administrator/items.php
@@ -2,49 +2,106 @@
 /**
  * Films model config
  */
+
 return array(
 	'title' => 'Items',
 	'single' => 'Item',
 	'model' => 'Item',
+	'form_width' => 600,
 	/**
 	 * The display columns
 	 */
 	'columns' => array(
-		'username',
-		'email',
-		'firstname' => array(
-			'title' => 'First name',
+		'name',
+		'category' => array(
+			'title' => 'category',
+			'relationship' => 'category',
+			'select' => "CONCAT((:table).id, ', ', (:table).name)",
+			'output' => function($value) {
+				list($id, $name) = explode(', ', $value, 2);
+
+				return HTML::link('admin/categories/'.$id, $name);
+			}
 		),
-		'lastname' => array(
-			'title' => 'Last name',
+		'img_url' => array(
+
+			'output' => function($value) {
+				if ($value) {
+					return HTML::image('upload/items/thumbs/small/'.$value);
+				}
+			},
+		),
+		'price' => array(
+			'type' => 'number',
+			'title' => 'Price',
+			'decimals' => 2, //optional, defaults to 0
+			'thousands_separator' => ',', //optional, defaults to ','
+			'decimal_separator' => '.', //optional, defaults to '.'
+		),
+		'quantity',
+		'positive_votes' => array(
+			'title' => 'positive Votes',
+		),
+		'negative_votes' => array(
+			'title' => 'natcasesort(array)egative Votes',
+		),
+		'created_at' => array(
+			'title' => 'created at',
+		),
+		'updated_at' => array(
+			'title' => 'updated at',
 		),
 	),
 	/**
 	 * The filter set
 	 */
 	'filters' => array(
-		'username',
-		'email',
-		'firstname' => array(
-			'title' => 'First name',
-		),
-		'lastname' => array(
-			'title' => 'Last name',
+		'name',
+		'created_at' => array(
+			'title' => 'created at',
+			'type' => 'date',
 		),
 	),
 	/**
 	 * The editable fields
 	 */
 	'edit_fields' => array(
-		'username',
-		'email' => array(
-			'type' => 'text',
+		'name',
+		'img_url' => array(
+			'title' => 'Image',
+		    'type' => 'image',
+		    'location' => public_path() . '/upload/items/originals/',
+		    'naming' => 'random',
+		    'length' => 20,
+		    'size_limit' => 2,
+		    'sizes' => array(
+		        array(100, 100, 'crop', public_path() . '/upload/items/thumbs/small/', 100),
+		        array(350, 350, 'fit', public_path() . '/upload/items/thumbs/medium/', 100),
+		        array(500, 500, 'fit', public_path() . '/upload/items/thumbs/full/', 100)
+			),
 		),
-		'firstname' => array(
-			'title' => 'First name',
+		'category_id' => array(
+		    'type' => 'enum',
+		    'title' => 'Category',
+		    'options' => array('4' => 'Pesho', 'Gosho', 'Tosho'),
 		),
-		'lastname' => array(
-			'title' => 'Last name',
+		'short_description' => array(
+			'type' => 'textarea',
+			'title' => 'Short description',
+		),
+		'description' => array(
+			'type' => 'textarea',
+			'title' => 'Description',
+		),
+		'price' => array(
+		    'type' => 'number',
+		    'title' => 'Price',
+		    'decimals' => 2, //optional, defaults to 0
+		    'thousands_separator' => ',', //optional, defaults to ','
+		    'decimal_separator' => '.', //optional, defaults to '.'
+		),
+		'quantity' => array(
+
 		),
 	),
 );
